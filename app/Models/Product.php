becd5027a49dcf8899a86fda16f35b4953ae7bd5
MuhammadHaweras <haweras.noor@gmail.com>
2026-04-02 20:59:44 +0500

    database and models initial version

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use function PHPUnit\Framework\returnArgument;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id', 'name', 'slug',
+        'description', 'price', 'stock',
+        'image', 'is_active'
+    ];
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function cartItems(){
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function orderItems(){
+        $this->hasMany(OrderItem::class);
+    }
+}
+
