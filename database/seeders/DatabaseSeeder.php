13774b63d2c96e480ee324f92bf52876d30e9cc4
MuhammadHaweras <haweras.noor@gmail.com>
2026-04-12 16:13:18 +0500

    ready for deployment

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
         $this->call(CategorySeeder::class);
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Super Admin',
+            'email' => 'admin@example.com',
+            'is_admin' => true,
         ]);
     }
     
